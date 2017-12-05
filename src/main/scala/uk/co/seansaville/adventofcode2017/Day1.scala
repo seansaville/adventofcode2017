423ee9c36940c08c1382fd9f9ea6d4f4302018d2
Sean Saville <sean.saville@hotmail.co.uk>
2017-12-05 21:14:52 +0000

    Add solution to day 1 (both parts)

---
+++ src/main/scala/uk/co/seansaville/adventofcode2017/Day1.scala
@@ -0,0 +1,41 @@
+package uk.co.seansaville.adventofcode2017
+
+import scala.io.Source
+
+object Day1 {
+  def inverseCaptchaPart1(captcha: List[Int]): Int = {
+    def go(list: List[Int], acc: Int): Int = list match {
+      case x1 :: x2 :: xs => if (x1 == x2) go(x2 :: xs, acc + x1) else go(x2 :: xs, acc)
+      case x :: List()    => if (x == captcha.head) acc + x else acc
+      case _              => 0
+    }
+
+    go(captcha, 0)
+  }
+
+  def inverseCaptchaPart2(captcha: Array[Int]): Int = {
+    def go(array: Array[Int], index: Int, acc: Int): Int = {
+      if (index == array.length) return acc
+
+      val otherIndex = (index + (captcha.length / 2)) % captcha.length
+
+      if (array(index) == array(otherIndex))
+        go(array, index + 1, acc + array(index))
+      else
+        go(array, index + 1, acc)
+    }
+
+    go(captcha, 0, 0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val fileBuffer = Source.fromFile("./src/main/resources/day1.input")
+    for (line <- fileBuffer.getLines) {
+      val captchaList = line.toList.map(_.asDigit)
+      val captchaArray = line.toCharArray.map(_.asDigit)
+      println(inverseCaptchaPart1(captchaList))
+      println(inverseCaptchaPart2(captchaArray))
+    }
+    fileBuffer.close
+  }
+}
