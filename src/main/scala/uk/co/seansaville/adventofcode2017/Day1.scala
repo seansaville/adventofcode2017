a3e062c5c633b31de7fde31b31e14227879f970e
Sean Saville <sean.saville@hotmail.co.uk>
2017-12-07 23:47:23 +0000

    Learnt some cool things, so made day 1 less gross

--- src/main/scala/uk/co/seansaville/adventofcode2017/Day1.scala
+++ src/main/scala/uk/co/seansaville/adventofcode2017/Day1.scala
@@ -3,38 +3,26 @@ package uk.co.seansaville.adventofcode2017
 import scala.io.Source
 
 object Day1 {
-  def inverseCaptchaPart1(captcha: List[Int]): Int = {
-    def go(list: List[Int], acc: Int): Int = list match {
-      case x1 :: x2 :: xs => if (x1 == x2) go(x2 :: xs, acc + x1) else go(x2 :: xs, acc)
-      case x :: List()    => if (x == captcha.head) acc + x else acc
-      case _              => 0
-    }
-
-    go(captcha, 0)
+  def inverseCaptcha1(captcha: Seq[Int]): Int = {
+    (captcha :+ captcha.head).sliding(2).filter(_.distinct.size == 1).map(_.head).sum
   }
 
-  def inverseCaptchaPart2(captcha: Array[Int]): Int = {
-    def go(index: Int, acc: Int): Int = {
-      if (index == captcha.length) return acc
+  def inverseCaptcha2(captcha: Seq[Int]): Int = {
+    val matchingDigits = for {
+      (digit, index) <- captcha.zipWithIndex
+      offset = (index + (captcha.length / 2)) % captcha.length
+      if digit == captcha(offset)
+    } yield digit
 
-      val otherIndex = (index + (captcha.length / 2)) % captcha.length
-
-      if (captcha(index) == captcha(otherIndex))
-        go(index + 1, acc + captcha(index))
-      else
-        go(index + 1, acc)
-    }
-
-    go(0, 0)
+    matchingDigits.sum
   }
 
   def main(args: Array[String]): Unit = {
     val fileBuffer = Source.fromFile("./src/main/resources/day1.input")
     for (line <- fileBuffer.getLines) {
-      val captchaList = line.toList.map(_.asDigit)
-      val captchaArray = line.toCharArray.map(_.asDigit)
-      println(inverseCaptchaPart1(captchaList))
-      println(inverseCaptchaPart2(captchaArray))
+      val captcha = line.toSeq.map(_.asDigit)
+      println(inverseCaptcha1(captcha))
+      println(inverseCaptcha2(captcha))
     }
     fileBuffer.close
   }
